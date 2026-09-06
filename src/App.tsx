a556da2966fa869ab7c5e38eb07b0c441bbd3a7f
leoArpit <arpitsrivastava287@gmail.com>
2026-09-06 15:25:27 +0530

    feat: add professional uploaded courses popup with 3s rotation and clean modal UI

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,7 @@ import { getEnrollmentsForUser } from './lib/videoEngagementService'
 import { useAuthStore, User } from './store/authStore'
 import MobileBottomNav from './components/MobileBottomNav'
 import WebinarVisitPopup from './components/WebinarVisitPopup'
+import NewCoursePopup from './components/NewCoursePopup'
 import CookieBanner from './components/CookieBanner'
 import { initGlobalHaptics } from './lib/haptics'
 import { getCookie } from './lib/cookieService'
@@ -212,6 +213,7 @@ export default function App() {
       </div>
       <MobileBottomNav />
       <WebinarVisitPopup />
+      <NewCoursePopup />
       <CookieBanner />
       <CustomToaster />
     </BrowserRouter>
